81691112caa3841724c7cb78096bc1b85ce2f98d
Sadadi Assani <sadadi.assani@laposte.fr>
2023-09-29 16:57:34 +0200

    wip

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
+import { DetailListePage } from './detail-liste/detail-liste.page';
 
 export const routes: Routes = [
   {
@@ -9,5 +10,21 @@ export const routes: Routes = [
     path: '',
     redirectTo: 'home',
     pathMatch: 'full',
+  },
+  {
+    path: 'detail-liste/:id',
+    loadComponent: () => import('./detail-liste/detail-liste.page').then( m => m.DetailListePage)
+  },
+  {
+    path:'details/:id',
+    component:DetailListePage
+  },
+  {
+    path: 'add-or-edit-list',
+    loadComponent: () => import('./add-or-edit-list/add-or-edit-list.page').then( m => m.AddOrEditListPage)
+  },
+  {
+    path: 'list',
+    loadComponent: () => import('./list/list.page').then( m => m.ListPage)
   },
 ];
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,95 @@
-import { Component } from '@angular/core';
-import { IonicModule } from '@ionic/angular';
+import { Component, OnInit, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { ActionSheetController, IonicModule } from '@ionic/angular';
+import { ListesCoursesService } from '../listes-courses.service';
+import { ListeCourses } from '../liste-courses';
+import {NavController  } from '@ionic/angular';
+import { RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
+import { SharePlugin } from '@capacitor/share';
+
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule, CommonModule, FormsModule,RouterLink]
 })
 export class HomePage {
-  constructor() {}
+  listesCourses;
+  /*  private listesCoursesService: ListesCoursesService = inject(ListesCoursesService);
+  listesCourses: ListeCourses[] = this.listesCoursesService.getListesCourses(); */
+ 
+
+  constructor(private nav1 :Router, private list: ListesCoursesService,
+    private actionSheetController:ActionSheetController ) {
+    this.listesCourses =this.list.getListesCourses();
+
+  }
+  
+  async presentActionSheet(liste: ListeCourses) {
+    const actionSheet = await this.actionSheetController.create({
+      header: `${liste.nom} ${liste.etat}`,
+      buttons: [
+        {
+          text: 'modifier',
+          icon: 'update',
+          handler: () => {
+            ;          },
+        },
+        {
+          text: 'Absent',
+          icon: 'eye-off-outline',
+          handler: () => {
+            ;
+          },
+        },
+        {
+          text: 'Supprimer',
+          icon: 'trash',
+          role: 'destructive',
+          handler: () => {
+            ;
+          },
+        },
+
+        {
+          text: 'Annuler',
+          icon: 'close',
+          role: 'cancel',
+          handler: () => {},
+        },
+      ],
+    });
+    actionSheet.present();
+  }
+  public actionSheetButtons = [
+    {
+      text: 'Delete',
+      role: 'destructive',
+      data: {
+        action: 'delete',
+      },
+    },
+    {
+      text: 'Share',
+      data: {
+        action: 'share',
+      },
+    },
+    {
+      text: 'Cancel',
+      role: 'cancel',
+      data: {
+        action: 'cancel',
+      },
+    },
+  ];
+  ngOnInit() {
+  }
+
+ 
 }
